776d349124f6498d6f977800f7ae175ac880b462
Michael Benaim <michael.benaim@external.grdf.fr>
2015-12-24 16:20:36 +0100

    Fixtures + config reinit_dev

--- bin/reinit_dev.sh
+++ bin/reinit_dev.sh
@@ -14,3 +14,5 @@ php app/console doctrine:schema:drop --force -e dev
 php app/console doctrine:schema:create -e dev
 php app/console cache:clear -e dev
 php app/console assets:install web --symlink -e dev
+
+php app/console doctrine:fixtures:load -n --fixtures= src/PortalBundle/DataFixtures/ORM
