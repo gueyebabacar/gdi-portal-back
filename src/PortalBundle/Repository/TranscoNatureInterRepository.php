220bce6d0b8e1810457ba8fa067330d4a1c2886d
Michael Benaim <michael.benaim@external.grdf.fr>
2015-12-24 15:45:55 +0100

    TranscoNatureInter + Repo + Test

---
+++ src/PortalBundle/Repository/TranscoNatureInterRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PortalBundle\Repository;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * TranscoNatureInterRepository
+ */
+class TranscoNatureInterRepository extends EntityRepository
+{
+}
