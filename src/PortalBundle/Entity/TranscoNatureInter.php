9e65b1c98be7540a64e8e36d213479c31443003e
Michael Benaim <michael.benaim@external.grdf.fr>
2015-12-24 15:45:55 +0100

    TranscoNatureInter + Repo + Test

---
+++ src/PortalBundle/Entity/TranscoNatureInter.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace PortalBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TranscoNatureInter
+ *
+ * @ORM\Table(name="transco_nature_inter")
+ * @ORM\Entity(repositoryClass="PortalBundle\Repository\TranscoNatureInterRepository")
+ */
+class TranscoNatureInter
+{
+    /**
+     * @var int
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * Code nature intervention OPTIC
+     *
+     * @var $opticNatCode
+     * @ORM\Column(type="string")
+     */
+    protected $opticNatCode;
+
+    /**
+     * Competence
+     *
+     * @var
+     * @ORM\Column(type="string")
+     */
+    protected $opticSkill;
+
+    /**
+     * Libelle nature intervention
+     *
+     * @var
+     * @ORM\Column(type="string")
+     */
+    protected $opticNatLabel;
+
+    /**
+     * Code NatOp
+     *
+     * @var
+     * @ORM\Column(type="string")
+     */
+    protected $pictrelNatOpCode;
+
+    /**
+     * Libelle NatOp
+     *
+     * @var
+     * @ORM\Column(type="string")
+     */
+    protected $pictrelNatOpLabel;
+
+    /**
+     * Libelle NatOp tronqué
+     *
+     * @var
+     * @ORM\Column(type="string")
+     */
+    protected $troncatedPictrelNatOpLabel;
+
+    /**
+     * app
+     *
+     * @var
+     * @ORM\Column(type="string")
+     */
+    protected $app;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOpticNatCode()
+    {
+        return $this->opticNatCode;
+    }
+
+    /**
+     * @param mixed $opticNatCode
+     * @return $this
+     */
+    public function setOpticNatCode($opticNatCode)
+    {
+        $this->opticNatCode = $opticNatCode;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOpticSkill()
+    {
+        return $this->opticSkill;
+    }
+
+    /**
+     * @param mixed $opticSkill
+     * @return $this
+     */
+    public function setOpticSkill($opticSkill)
+    {
+        $this->opticSkill = $opticSkill;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOpticNatLabel()
+    {
+        return $this->opticNatLabel;
+    }
+
+    /**
+     * @param mixed $opticNatLabel
+     * @return $this
+     */
+    public function setOpticNatLabel($opticNatLabel)
+    {
+        $this->opticNatLabel = $opticNatLabel;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPictrelNatOpCode()
+    {
+        return $this->pictrelNatOpCode;
+    }
+
+    /**
+     * @param mixed $pictrelNatOpCode
+     * @return $this
+     */
+    public function setPictrelNatOpCode($pictrelNatOpCode)
+    {
+        $this->pictrelNatOpCode = $pictrelNatOpCode;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPictrelNatOpLabel()
+    {
+        return $this->pictrelNatOpLabel;
+    }
+
+    /**
+     * @param mixed $pictrelNatOpLabel
+     * @return $this
+     */
+    public function setPictrelNatOpLabel($pictrelNatOpLabel)
+    {
+        $this->pictrelNatOpLabel = $pictrelNatOpLabel;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTroncatedPictrelNatOpLabel()
+    {
+        return $this->troncatedPictrelNatOpLabel;
+    }
+
+    /**
+     * @param mixed $troncatedPictrelNatOpLabel
+     * @return $this
+     */
+    public function setTroncatedPictrelNatOpLabel($troncatedPictrelNatOpLabel)
+    {
+        $this->troncatedPictrelNatOpLabel = $troncatedPictrelNatOpLabel;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApp()
+    {
+        return $this->app;
+    }
+
+    /**
+     * @param mixed $app
+     * @return $this
+     */
+    public function setApp($app)
+    {
+        $this->app = $app;
+        return $this;
+    }
+}
+
---
+++ src/PortalBundle/Tests/Entity/TranscoNatureInterTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PortalBundle\Tests\Entity;
+
+use PortalBundle\Entity\TranscoNatureInter;
+
+class TranscoNatureInterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @group pop
+     * @group entity
+     */
+    public function testTranscoNatureInterSettersGetters()
+    {
+        $data = array(
+            'id' => 1,
+            'opticNatCode' => 'ROBI',
+            'opticSkill' => 'Maintenance Robinet',
+            'opticNatLabel' => 'Inspection robinet reseau',
+            'pictrelNatOpCode' => 'AA',
+            'pictrelNatLabel' => 'Inspection robinet reseau',
+            'troncatedPictrelNatOpLabel' => 'Inspection robinet reseau',
+            'app' => 1,
+        );
+
+        $transcoNatureInter = new TranscoNatureInter();
+
+        $transcoNatureInter->setId($data['id']);
+        $transcoNatureInter->setOpticNatCode($data['opticNatCode']);
+        $transcoNatureInter->setOpticSkill($data['opticSkill']);
+        $transcoNatureInter->setOpticNatLabel($data['opticNatLabel']);
+        $transcoNatureInter->setPictrelNatOpCode($data['pictrelNatOpCode']);
+        $transcoNatureInter->setPictrelNatOpLabel($data['pictrelNatLabel']);
+        $transcoNatureInter->setTroncatedPictrelNatOpLabel($data['troncatedPictrelNatOpLabel']);
+        $transcoNatureInter->setApp($data['app']);
+
+        $this->assertEquals($data['id'], $transcoNatureInter->getId());
+        $this->assertEquals($data['opticNatCode'], $transcoNatureInter->getOpticNatCode());
+        $this->assertEquals($data['opticNatLabel'], $transcoNatureInter->getOpticNatLabel());
+        $this->assertEquals($data['pictrelNatOpCode'], $transcoNatureInter->getPictrelNatOpCode());
+        $this->assertEquals($data['pictrelNatLabel'], $transcoNatureInter->getPictrelNatOpLabel());
+        $this->assertEquals($data['troncatedPictrelNatOpLabel'], $transcoNatureInter->getTroncatedPictrelNatOpLabel());
+        $this->assertEquals($data['app'], $transcoNatureInter->getApp());
+    }
+}
