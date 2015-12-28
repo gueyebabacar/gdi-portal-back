37c84b7d9ce03ba94884f6d018377f57ac2381e8
Michael Benaim <michael.benaim@external.grdf.fr>
2015-12-28 17:10:46 +0100

    Added TranscoNatureOpe Type +  Repo + Service

---
+++ src/PortalBundle/Repository/TranscoNatureOpeRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PortalBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * TranscoNatureOpeRepository
+ */
+class TranscoNatureOpeRepository extends EntityRepository
+{
+}
