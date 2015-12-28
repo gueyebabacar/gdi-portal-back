4355dba0f4fc9781ea50f0ac2a9621e5ac355a91
Michael Benaim <michael.benaim@external.grdf.fr>
2015-12-28 17:10:46 +0100

    Added TranscoNatureOpe Type +  Repo + Service

---
+++ src/PortalBundle/Form/TranscoNatureOpeType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PortalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TranscoNatureOpeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('workType')
+            ->add('gammeGroup')
+            ->add('counter')
+            ->add('purpose')
+            ->add('natureInterCode')
+            ->add('programmingMode')
+            ->add('segmentationName')
+            ->add('segmentationValue')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PortalBundle\Entity\TranscoNatureOpe'
+        ));
+    }
+}
