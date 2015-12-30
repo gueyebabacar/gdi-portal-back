6be8410a9b044d8e018e57768b6c824aef857e07
Michael Benaim <michael.benaim@external.grdf.fr>
2015-12-30 18:31:42 +0100

    TranscoNatureInter repo

--- src/PortalBundle/Repository/TranscoNatureInterRepository.php
+++ src/PortalBundle/Repository/TranscoNatureInterRepository.php
@@ -9,4 +9,35 @@ use Doctrine\ORM\EntityRepository;
  */
 class TranscoNatureInterRepository extends EntityRepository
 {
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function findCodeNatIntFromCodeNatOp(array $data)
+    {
+        $qb = $this->createQueryBuilder("tni");
+        $qb->select("tni.opticNatCode")
+            ->where("tni.pictrelNatOpCode = :natOp")
+            ->setParameter('natOp', $data['criteria'][0]['value']);
+
+        return $qb->getQuery()->getOneOrNullResult();
+    }
+
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function findCodeNatopFromCodeNatInt(array $data)
+    {
+        $qb = $this->createQueryBuilder("tni");
+        $qb->select("tni.pictrelNatOpCode")
+            ->where("tni.opticNatCode = :natInt")
+            ->setParameter('natInt', $data['criteria'][0]['value']);
+
+        return $qb->getQuery()->getOneOrNullResult();
+    }
+
+
 }
