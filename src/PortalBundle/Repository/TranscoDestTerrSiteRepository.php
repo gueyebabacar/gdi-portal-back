4ced15dd98f217af5dc158ff4de373e3ff37d305
unknown <bgueye@maltem.com>
2015-12-30 17:42:56 +0100

    Transco destinataire site repository

--- src/PortalBundle/Repository/TranscoDestTerrSiteRepository.php
+++ src/PortalBundle/Repository/TranscoDestTerrSiteRepository.php
@@ -7,4 +7,17 @@ use Doctrine\ORM\EntityRepository;
 class TranscoDestTerrSiteRepository extends EntityRepository
 {
 
+    public  function findByIdRefOp($idRefStructureOp)
+    {
+        $qb = $this->createQueryBuilder('t');
+
+        $qb
+            ->where('t.idRefStructureOp = :idRefStructureOp')
+            ->setParameter('idRefStructureOp', $idRefStructureOp);
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
 }
