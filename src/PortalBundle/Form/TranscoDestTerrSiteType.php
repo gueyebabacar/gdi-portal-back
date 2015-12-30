00603b36c06bae10a8311eb00ecbb463ba1ceef9
unknown <bgueye@maltem.com>
2015-12-30 16:47:47 +0100

    Transco Destinataire Territory Site form

---
+++ src/PortalBundle/Form/TranscoDestTerrSiteType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PortalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TranscoDestTerrSiteType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('territory')
+            ->add('adressee')
+            ->add('site')
+            ->add('idRefStructureOp')
+            ->add('pr')
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PortalBundle\Entity\TranscoDestTerrSite'
+        ));
+    }
+}
