e557005adc20a96f6ba61d02d22f9322d90172c0
Michael Benaim <michael.benaim@external.grdf.fr>
2015-12-31 15:51:55 +0100

    TranscoNatureInterRepo test

---
+++ src/PortalBundle/Tests/Repository/TranscoNatureInterRepositoryTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace PortalBundle\Tests\Repository;
+
+use PortalBundle\Entity\TranscoNatureInter;
+use PortalBundle\Repository\TranscoNatureInterRepository;
+use PortalBundle\Tests\BaseWebTestCase;
+
+class TranscoNatureInterRepositoryTest extends BaseWebTestCase
+{
+    /**
+     * @var TranscoNatureInterRepository
+     */
+    protected $transcoNatureInterRepo;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->transcoNatureInterRepo = $this->em->getRepository('PortalBundle:TranscoNatureInter');
+    }
+
+    public function testfindCodeNatIntFromCodeNatOp()
+    {
+        $data['criteria'][0]['value'] = "NATOPCODE";
+        $this->insertTranscoNatureInter();
+        $result = $this->transcoNatureInterRepo->findCodeNatIntFromCodeNatOp($data);
+        $this->assertEquals(reset($result[0]), 'AAA');
+    }
+
+    public function testfindCodeNatopFromCodeNatInt()
+    {
+        $data['criteria'][0]['value'] = "AAA";
+        $this->insertTranscoNatureInter();
+        $result = $this->transcoNatureInterRepo->findCodeNatopFromCodeNatInt($data);
+        $this->assertEquals(reset($result[0]), "NATOPCODE");
+    }
+
+    /**
+     *insertTranscoNatureInter
+     */
+    private function insertTranscoNatureInter()
+    {
+        $transcoNatureInter = new TranscoNatureInter();
+
+        $transcoNatureInter->setOpticNatCode('AAA');
+        $transcoNatureInter->setOpticSkill('optic skill');
+        $transcoNatureInter->setOpticNatLabel('optic nat label');
+        $transcoNatureInter->setPictrelNatOpCode('NATOPCODE');
+        $transcoNatureInter->setPictrelNatOpLabel('nat op label');
+        $transcoNatureInter->setTroncatedPictrelNatOpLabel('troncated nat op label');
+        $transcoNatureInter->setApp(0);
+
+        $this->em->persist($transcoNatureInter);
+        $this->em->flush();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+}
