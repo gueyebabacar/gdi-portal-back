cd5d85d70ea938b725d0c7c76131415b8e7e919f
Michael Benaim <michael.benaim@external.grdf.fr>
2015-12-31 14:45:42 +0100

    TranscoNatureOpeRepository

--- src/PortalBundle/Repository/TranscoNatureOpeRepository.php
+++ src/PortalBundle/Repository/TranscoNatureOpeRepository.php
@@ -9,4 +9,49 @@ use Doctrine\ORM\EntityRepository;
  */
 class TranscoNatureOpeRepository extends EntityRepository
 {
+    const TYPE_DE_TRAVAIL = "TypeDeTravail";
+    const GROUPE_DE_GAME = "GroupeDeGame";
+    const COMPTEUR = "Compteur";
+
+    public function getCodeNatureIntervention3(array $data)
+    {
+        $qb = $this->createQueryBuilder('tno');
+        $qb->select('tno.natureInterCode');
+        foreach ($data['criteria'] as $item) {
+            if ($item['name'] === self::TYPE_DE_TRAVAIL) {
+                $qb->andWhere('tno.workType = :workType')
+                    ->setParameter('workType', $item['value']);
+            }
+            if ($item['name'] === self::GROUPE_DE_GAME) {
+                $qb->andWhere('tno.gammeGroup = :gammeGroup')
+                    ->setParameter('gammeGroup', $item['value']);
+            }
+            if ($item['name'] === self::COMPTEUR) {
+                $qb->andWhere('tno.counter = :counter')
+                    ->setParameter('counter', $item['value']);
+            }
+        }
+        return $qb->getQuery()->getArrayResult();
+    }
+
+    public function getModeProgrammation(array $data)
+    {
+        $qb = $this->createQueryBuilder('tno');
+        $qb->select('tno.programmingMode');
+        foreach ($data['criteria'] as $item) {
+            if ($item['name'] === self::TYPE_DE_TRAVAIL) {
+                $qb->andWhere('tno.workType = :workType')
+                    ->setParameter('workType', $item['value']);
+            }
+            if ($item['name'] === self::GROUPE_DE_GAME) {
+                $qb->andWhere('tno.gammeGroup = :gammeGroup')
+                    ->setParameter('gammeGroup', $item['value']);
+            }
+            if ($item['name'] === self::COMPTEUR) {
+                $qb->andWhere('tno.counter = :counter')
+                    ->setParameter('counter', $item['value']);
+            }
+        }
+        return $qb->getQuery()->getArrayResult();
+    }
 }
