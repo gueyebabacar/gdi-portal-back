fce460fa3b53ccbfbb13661aed67edc411435a4a
unknown <bgueye@maltem.com>
2015-12-31 14:46:34 +0100

    Transco Destinataire Site Form Test

---
+++ src/PortalBundle/Tests/Form/TranscoDestTerrSiteTypeTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace PortalBundle\Tests\Form;
+
+use PortalBundle\Form\TranscoDestTerrSiteType;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class TranscoDestTerrSiteTypeTest extends TypeTestCase
+{
+    public function testSubmitValidData()
+    {
+        $formData = array(
+            'test' => 'Lorem Ipsum',
+            'test2' => 'Lorem Ipsum',
+        );
+        $type = new TranscoDestTerrSiteType();
+        $form = $this->factory->create($type);
+        // submit the data to the form directly
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key) {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+}
