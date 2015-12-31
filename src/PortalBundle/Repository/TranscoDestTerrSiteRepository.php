778f87fb7098c1ad26cac10bc92aa693c7148b9a
unknown <bgueye@maltem.com>
2015-12-31 16:08:40 +0100

    transco destinataire territory site full repository

--- src/PortalBundle/Repository/TranscoDestTerrSiteRepository.php
+++ src/PortalBundle/Repository/TranscoDestTerrSiteRepository.php
@@ -20,4 +20,58 @@ class TranscoDestTerrSiteRepository extends EntityRepository
             ->getResult();
     }
 
+
+    public  function findByTerritory($territory)
+    {
+        $qb = $this->createQueryBuilder('t');
+
+        $qb
+            ->where('t.territory = :territory')
+            ->setParameter('territory', $territory);
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
+
+    public  function findBySite($site)
+    {
+        $qb = $this->createQueryBuilder('t');
+
+        $qb
+            ->where('t.site = :site')
+            ->setParameter('site', $site);
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
+    public  function findByAdressee($adressee)
+    {
+        $qb = $this->createQueryBuilder('t');
+
+        $qb
+            ->where('t.adressee = :adressee')
+            ->setParameter('adressee', $adressee);
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
+
+
+    public  function findByPr($pr)
+    {
+        $qb = $this->createQueryBuilder('t');
+
+        $qb
+            ->where('t.pr = :pr')
+            ->setParameter('pr', $pr);
+
+        return $qb
+            ->getQuery()
+            ->getResult();
+    }
 }
