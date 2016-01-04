41c9af95a19ef7dd4c65acd0cbcd4f60e0c09e3e
unknown <bgueye@maltem.com>
2016-01-04 11:16:40 +0100

    TranscoDestiTerrSite  Repository

--- src/PortalBundle/Repository/TranscoDestTerrSiteRepository.php
+++ src/PortalBundle/Repository/TranscoDestTerrSiteRepository.php
@@ -21,57 +21,85 @@ class TranscoDestTerrSiteRepository extends EntityRepository
     }
 
 
-    public  function findByTerritory($territory)
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function findTerritoryFromAtg(array $data)
     {
-        $qb = $this->createQueryBuilder('t');
+        $qb = $this->createQueryBuilder("t");
+        $qb->select("t.territory")
+            ->where("t.idRefStructureOp = :idRefStructureOp")
+            ->setParameter('idRefStructureOp', $data['criteria'][0]['value']);
 
-        $qb
-            ->where('t.territory = :territory')
-            ->setParameter('territory', $territory);
-
-        return $qb
-            ->getQuery()
-            ->getResult();
+        return $qb->getQuery()->getArrayResult();
     }
 
 
-    public  function findBySite($site)
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public  function findAdresseeFromAtg(array $data)
     {
-        $qb = $this->createQueryBuilder('t');
+        $qb = $this->createQueryBuilder("t");
+        $qb->select("t.adressee")
+            ->where("t.idRefStructureOp = :idRefStructureOp")
+            ->setParameter('idRefStructureOp', $data['criteria'][0]['value']);
 
-        $qb
-            ->where('t.site = :site')
-            ->setParameter('site', $site);
-
-        return $qb
-            ->getQuery()
-            ->getResult();
+        return $qb->getQuery()->getArrayResult();
     }
 
-    public  function findByAdressee($adressee)
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public  function findPrFromAtg(array $data)
     {
-        $qb = $this->createQueryBuilder('t');
+        $qb = $this->createQueryBuilder("t");
+        $qb->select("t.pr")
+            ->where("t.idRefStructureOp = :idRefStructureOp")
+            ->setParameter('idRefStructureOp', $data['criteria'][0]['value']);
 
-        $qb
-            ->where('t.adressee = :adressee')
-            ->setParameter('adressee', $adressee);
+        return $qb->getQuery()->getArrayResult();
+    }
 
-        return $qb
-            ->getQuery()
-            ->getResult();
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public  function findAtgFromTerritory(array $data)
+    {
+        $qb = $this->createQueryBuilder("t");
+        $qb->select("t.idRefStructureOp")
+            ->where("t.territory = :territory")
+            ->setParameter('territory', $data['criteria'][0]['value']);
+
+        return $qb->getQuery()->getArrayResult();
     }
 
 
-    public  function findByPr($pr)
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public  function findAtgFromAdressee(array $data)
     {
-        $qb = $this->createQueryBuilder('t');
+        $qb = $this->createQueryBuilder("t");
+        $qb->select("t.idRefStructureOp")
+            ->where("t.adressee = :adressee")
+            ->setParameter('adressee', $data['criteria'][0]['value']);
 
-        $qb
-            ->where('t.pr = :pr')
-            ->setParameter('pr', $pr);
-
-        return $qb
-            ->getQuery()
-            ->getResult();
+        return $qb->getQuery()->getArrayResult();
     }
 }
+
+
+
+
+
