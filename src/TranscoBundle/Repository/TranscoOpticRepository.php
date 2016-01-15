df4fa0f43555f29606f5ee214edf90029a2f8c58
gueye <babacar.gueye@external.grdf.fr>
2016-01-15 16:16:32 +0100

    TranscoOpticRepository

--- src/TranscoBundle/Repository/TranscoOpticRepository.php
+++ src/TranscoBundle/Repository/TranscoOpticRepository.php
@@ -13,27 +13,35 @@ use Doctrine\ORM\EntityRepository;
  */
 class TranscoOpticRepository extends EntityRepository
 {
+    // GMAO constant
     const TYPE_DE_TRAVAIL = "TypeDeTravail";
     const GROUPE_DE_GAMME = "GroupeDeGamme";
     const COMPTEUR = "Compteur";
+
+    // Disco Constant
+    const CODE_NAT_Op = "CodeNatureOperation";
+    const CODE_OBJECT = "CodeObjet";
+
 
 
     public function findDelegationOT(array $data)
     {
         $qb = $this->createQueryBuilder('t');
         $qb->select('t.codeNatInter, t.finalCode, t.segmentationCode, t.programmingMode');
+        $qb->leftJoin('t.gmao', 'gmao')
+            ->addSelect('gmao');
 
         foreach ($data['criteria'] as $item) {
             if ($item['name'] === self::TYPE_DE_TRAVAIL) {
-                $qb->andWhere('t.workType = :workType')
+                $qb->andWhere('gmao.workType = :workType')
                     ->setParameter('workType', $item['value']);
             }
             if ($item['name'] === self::GROUPE_DE_GAMME) {
-                $qb->andWhere('t.gammeGroup = :gammeGroup')
+                $qb->andWhere('gmao.gammeGroup = :gammeGroup')
                     ->setParameter('gammeGroup', $item['value']);
             }
             if ($item['name'] === self::COMPTEUR) {
-                $qb->andWhere('t.counter = :counter')
+                $qb->andWhere('gmao.counter = :counter')
                     ->setParameter('counter', $item['value']);
             }
         }
@@ -43,8 +51,22 @@ class TranscoOpticRepository extends EntityRepository
 
     public function findDelegationBI(array $data)
     {
+        $qb = $this->createQueryBuilder('t');
+        $qb->leftJoin('t.disco', 'disco')
+            ->addSelect('disco');
+        $qb->select('t.codeNatInter, t.finalCode, t.segmentationCode, t.programmingMode');
 
+        foreach ($data['criteria'] as $item) {
+            if ($item['name'] === self::CODE_NAT_Op) {
+                $qb->andWhere('disco.natOp = :natOp')
+                    ->setParameter('natOp', $item['value']);
+            }
+            if ($item['name'] === self::CODE_OBJECT) {
+                $qb->andWhere('disco.codeObject = :codeObject')
+                    ->setParameter('codeObject', $item['value']);
+            }
+        }
+        return $qb->getQuery()->getArrayResult();
     }
-
 
 }
