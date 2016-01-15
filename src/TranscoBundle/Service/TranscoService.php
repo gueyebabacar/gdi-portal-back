d95d35e057e8df88ed94bc0630752935edd94a71
Michael Benaim <michael.benaim@external.grdf.fr>
2016-01-15 15:10:22 +0100

    Update Transco

--- src/TranscoBundle/Service/TranscoService.php
+++ src/TranscoBundle/Service/TranscoService.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityManager;
 use JMS\DiExtraBundle\Annotation as DI;
 use TranscoBundle\Repository\TranscoAgenceRepository;
 use TranscoBundle\Repository\TranscoDiscoRepository;
+use TranscoBundle\Repository\TranscoOpticRepository;
 
 /**
  * Class TranscoService
@@ -26,18 +27,12 @@ class TranscoService
      * @var TranscoDiscoRepository
      */
     public $transcoDiscoRepo;
-//
-//    /**
-//     * @DI\Inject("service.transco.gmao.repo")
-//     * @var TranscoGmaoRepository
-//     */
-//    public $transcoGmaoRepo;
-//
-//    /**
-//     * @DI\Inject("service.transco.optic.repo")
-//     * @var TranscoOpticRepository
-//     */
-//    public $transcoOpticRepo;
+
+    /**
+     * @DI\Inject("service.transco.optic.repo")
+     * @var TranscoOpticRepository
+     */
+    public $transcoOpticRepo;
 
     /**
      * @DI\Inject("service.transcoq.agence.repo")
@@ -50,20 +45,19 @@ class TranscoService
      * @param EntityManager $em
      *
      * @param $transcoDiscoRepo
-     * @param $transcoGmaoRepo
      * @param $transcoOpticRepo
      * @param $transcoAgenceRepo
+     * @internal param $transcoGmaoRepo
      * @DI\InjectParams({
      *     "em" = @DI\Inject("doctrine.orm.entity_manager"),
      *     "transcoDiscoRepo" = @DI\Inject("service.transco.disco.repo"),
      *     "transcoAgenceRepo" = @DI\Inject("service.transco.agence.repo"),
      * })
      */
-    public function __construct($em, $transcoDiscoRepo, $transcoGmaoRepo, $transcoOpticRepo, $transcoAgenceRepo)
+    public function __construct($em, $transcoDiscoRepo, $transcoOpticRepo, $transcoAgenceRepo)
     {
         $this->em = $em;
         $this->transcoDiscoRepo = $transcoDiscoRepo;
-        $this->transcoGmaoRepo = $transcoGmaoRepo;
         $this->transcoOpticRepo = $transcoOpticRepo;
         $this->transcoAgenceRepo = $transcoAgenceRepo;
     }
