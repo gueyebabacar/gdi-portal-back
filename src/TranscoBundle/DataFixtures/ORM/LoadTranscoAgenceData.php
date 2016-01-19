04c481e44ade6dd7e57465571b65a3adb9283e41
Michael Benaim <michael.benaim@external.grdf.fr>
2016-01-19 11:57:40 +0100

    Added Fixtures for Agence

--- src/TranscoBundle/DataFixtures/ORM/LoadTranscoAgenceData.php
+++ src/TranscoBundle/DataFixtures/ORM/LoadTranscoAgenceData.php
@@ -2,44 +2,24 @@
 
 namespace TranscoBundle\DataFixtures\ORM;
 
-use Apoutchika\LoremIpsumBundle\Services\LoremIpsum;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use TranscoBundle\Entity\TranscoNatureOpe;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use TranscoBundle\Entity\TranscoAgence;
 
-class LoadTranscoNatureOpeData implements FixtureInterface, ContainerAwareInterface
+class LoadTranscoAgenceData implements FixtureInterface
 {
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
-
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
-    }
-
     public function load(ObjectManager $manager)
     {
-
-        /** @var LoremIpsum $loremIpsum */
-        $loremIpsum = $this->container->get("apoutchika.lorem_ipsum");
-
         for ($i = 0; $i < 20; $i++) {
-            $transcoNatureOpe = new TranscoNatureOpe();
+            $transcoAgence = new TranscoAgence();
 
-            $transcoNatureOpe->setWorkType($loremIpsum->getWords(1));
-            $transcoNatureOpe->setGammeGroup(strtoupper($loremIpsum->getWords(1)));
-            $transcoNatureOpe->setPurpose($loremIpsum->getWords(1));
-            $transcoNatureOpe->setCounter($i + 1);
-            $transcoNatureOpe->setSegmentationValue($loremIpsum->getWords(3));
-            $transcoNatureOpe->setSegmentationName($loremIpsum->getWords(3));
-            $transcoNatureOpe->setProgrammingMode($loremIpsum->getWords(3));
-            $transcoNatureOpe->setNatureInterCode(strtoupper($loremIpsum->getWords(1)));
-
-            $manager->persist($transcoNatureOpe);
+            $transcoAgence->setAgenceLabel('AgenceLabel-'.$i);
+            $transcoAgence->setCenter('Centre-'.$i);
+            $transcoAgence->setCodeAgence('CodeAgence-'.$i);
+            $transcoAgence->setDestinataire('Destinataire-'.$i);
+            $transcoAgence->setInseeCode('INSEE-'.$i);
+            $transcoAgence->setPr('ATG-'.$i);
+            $manager->persist($transcoAgence);
         }
 
         $manager->flush();
