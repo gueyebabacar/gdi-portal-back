2e5d0c961088a3eac48abb2a6f3c201e1c63ac8a
Michael Benaim <michael.benaim@external.grdf.fr>
2016-01-20 11:29:26 +0100

    TranscoServiceTest skipped

--- src/TranscoBundle/Tests/Service/TranscoServiceTest.php
+++ src/TranscoBundle/Tests/Service/TranscoServiceTest.php
@@ -1,15 +1,83 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: michaelbenaim
- * Date: 20/01/2016
- * Time: 10:40
- */
 
 namespace TranscoBundle\Tests\Service;
 
+use Doctrine\ORM\EntityManager;
+use Prophecy\Prophecy\ObjectProphecy;
+use Prophecy\Prophet;
+use TranscoBundle\Repository\TranscoAgenceRepository;
+use TranscoBundle\Repository\TranscoDiscoRepository;
+use TranscoBundle\Repository\TranscoOpticRepository;
+use TranscoBundle\Service\TranscoService;
 
 class TranscoServiceTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Prophet
+     */
+    private $prophet;
 
+    /**
+     * @var ObjectProphecy
+     */
+    private $emProphecy;
+
+    /**
+     * @var ObjectProphecy
+     */
+    private $transcoDiscoRepoProphecy;
+
+    /**
+     * @var ObjectProphecy
+     */
+    private $transcoOpticRepoProphecy;
+
+    /**
+     * @var ObjectProphecy
+     */
+    private $transcoAgenceRepoProphecy;
+
+    /**
+     * @var TranscoService
+     */
+    private $transcoService;
+
+
+
+    public function setUp()
+    {
+        $this->prophet = new Prophet();
+        $this->emProphecy = $this->prophet->prophesize(EntityManager::class);
+        $this->transcoAgenceRepoProphecy = $this->prophet->prophesize(TranscoAgenceRepository::class);
+        $this->transcoDiscoRepoProphecy = $this->prophet->prophesize(TranscoDiscoRepository::class);
+        $this->transcoOpticRepoProphecy = $this->prophet->prophesize(TranscoOpticRepository::class);
+
+        /** @var EntityManager $em */
+        $em = $this->emProphecy->reveal();
+        $this->transcoService = new TranscoService($em);
+    }
+
+    public function testGetDelegationBUResponse()
+    {
+        $this->markTestIncomplete();
+        $criteria = [
+            [
+                "name" => "TypeDeTravail",
+                "value" => "INS"
+            ]
+        ];
+
+        $result = [
+            [
+                "codeNatInter" => "codeNat"
+            ]
+        ];
+
+        $this->transcoOpticRepoProphecy
+            ->findDelegationBI($criteria)
+            ->willReturn($result)
+            ->shouldBeCalled();
+
+        $return = $this->transcoService->getDelegationBiResponse($criteria);
+    }
 }
