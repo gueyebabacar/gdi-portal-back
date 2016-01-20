a642e6dd8a054f170502ac738af44f45b772f6cb
Michael Benaim <michael.benaim@external.grdf.fr>
2016-01-20 11:28:37 +0100

    Clean former transpose

---
+++ src/TranscoBundle/Tests/Service/TranscoServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelbenaim
+ * Date: 20/01/2016
+ * Time: 10:40
+ */
+
+namespace TranscoBundle\Tests\Service;
+
+
+class TranscoServiceTest extends \PHPUnit_Framework_TestCase
+{
+
+}
