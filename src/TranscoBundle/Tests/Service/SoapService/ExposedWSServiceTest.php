94f07f187122f4efea6338e77070097f12682b2b
Michael Benaim <michael.benaim@external.grdf.fr>
2016-01-20 11:28:37 +0100

    Clean former transpose

---
+++ src/TranscoBundle/Tests/Service/SoapService/ExposedWSServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelbenaim
+ * Date: 19/01/2016
+ * Time: 15:49
+ */
+
+namespace TranscoBundle\Tests\Service\SoapService;
+
+
+class ExposedWSServiceTest extends \PHPUnit_Framework_TestCase
+{
+
+}
