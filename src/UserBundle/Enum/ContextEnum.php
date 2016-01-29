8439aaba53388a5fa40ce7cfba3342c9e05b68c0
gueye <babacar.gueye@external.grdf.fr>
2016-01-29 16:48:44 +0100

    UserBundle ContextEnum

---
+++ src/UserBundle/Enum/ContextEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace UserBundle\Enum;
+
+/**
+ * @package UserBundle\Enum
+ */
+class ContextEnum extends BaseEnum
+{
+    const REGION_CONTEXT = 'REGION';
+    const AGENCY_CONTEXT = 'AGENCE';
+    const NATIONAL_CONTEXT = 'NATIONAL';
+}
