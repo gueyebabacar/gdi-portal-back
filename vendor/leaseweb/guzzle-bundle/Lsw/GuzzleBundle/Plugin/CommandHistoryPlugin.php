2c94e60e5a283b972c52c70208c7be8b2a4c3491
Michael Benaim <michael.benaim@external.grdf.fr>
2016-02-03 09:43:59 +0100

    Added Guzzle + configured reporting

---
+++ vendor/leaseweb/guzzle-bundle/Lsw/GuzzleBundle/Plugin/CommandHistoryPlugin.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Lsw\GuzzleBundle\Plugin;
+
+use Guzzle\Service\Command\CommandInterface;
+
+use Guzzle\Common\Event;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Guzzle\Http\Message\RequestInterface;
+
+/**
+ * Maintains a list of commands sent using a client
+ */
+class CommandHistoryPlugin implements EventSubscriberInterface, \IteratorAggregate, \Countable
+{
+	/** @var array Commands that have passed through the plugin */
+	protected $transactions = array();
+
+	public static function getSubscribedEvents()
+	{
+		return array(
+			'command.before_send' => array('onCommandBeforeSent', 9999),
+			'request.sent' => array('onRequestSent', 9999),	
+		);
+	}
+
+	/**
+	 * Add a request to the history
+	 *
+	 * @param RequestInterface $request Request to add
+	 * @param CommandInterface $command Command to add
+	 *
+	 * @return CommandHistoryPlugin
+	 */
+	public function add(RequestInterface $request, CommandInterface $command = null)
+	{
+		$key = spl_object_hash($request);
+		if (!key_exists($key, $this->transactions)) {
+			$this->transactions[$key] = array(
+				'request'=>null,
+				'command'=>null,
+			);
+		}		
+		
+		$this->transactions[$key]['request'] = $request;
+		if ($command) {
+			$this->transactions[$key]['command'] = $command;
+		}
+		
+		return $this;
+	}
+
+	/**
+	 * Get the requests in the history
+	 *
+	 * @return \ArrayIterator
+	 */
+	public function getIterator()
+	{
+		return new \ArrayIterator($this->transactions);
+	}
+
+    /**
+     * Get the number of requests in the history
+     *
+     * @return int
+     */
+    public function count()
+    {
+        return count($this->transactions);
+    }
+	
+	public function onCommandBeforeSent(Event $event)
+	{
+		$request = $event['command']->getRequest();
+		$this->add($request, $event['command']);
+	}
+	
+	public function onRequestSent(Event $event)
+	{
+		$this->add($event['request']);
+	}
+	
+}
---
+++ vendor/leaseweb/guzzle-bundle/Lsw/GuzzleBundle/Plugin/JsonObjectsPlugin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Lsw\GuzzleBundle\Plugin;
+
+use Guzzle\Service\Command\CommandInterface;
+
+use Guzzle\Common\Event;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Support JSON objects as return value in Guzzle
+ */
+class JsonObjectsPlugin implements EventSubscriberInterface
+{
+	private $body;
+	
+	public static function getSubscribedEvents()
+	{
+		return array('command.after_send' => array('onCommandAfterSent', 9999));
+	}
+
+	public function onCommandAfterSent(Event $event)
+	{
+		$command = $event['command'];
+		$response = $command->getRequest()->getResponse();
+		if (!$response || !$response->isContentType('json') || $response->isError()) return;
+		$this->body = $response->getBody(true);
+		$command->setResult($this->json());
+	}
+	
+	public function json()
+	{
+		$data = json_decode((string) $this->body);
+		if (JSON_ERROR_NONE !== json_last_error()) {
+			throw new RuntimeException('Unable to parse response body into JSON: ' . json_last_error());
+		}
+	
+		return $data;
+	}
+	
+}
