708197dc8b10af2b551bfdbb7ec66732175326c8
gueye <babacar.gueye@external.grdf.fr>
2016-02-08 15:13:07 +0100

    UserBundle userRepository

--- src/UserBundle/Repository/UserRepository.php
+++ src/UserBundle/Repository/UserRepository.php
@@ -4,8 +4,25 @@ namespace UserBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+/**
+ * Class UserRepository
+ * @package UserBundle\Repository
+ */
 class UserRepository extends EntityRepository
 {
+    public function getUserAttributes()
+    {
+        $qb = $this->createQueryBuilder('user');
+        $qb
+            ->select('user.firstName, user.lastName, user.entity, user.username, role.label as userRole, agency.code as userAgency, user.enabled')
+            ->leftJoin('user.role', 'role')
+            ->leftJoin('user.agency', 'agency');
+
+        $q = $qb->getQuery();
+
+        return $q->getArrayResult();
+    }
+
     public function getProfiles()
     {
         $qb = $this->createQueryBuilder('user');
