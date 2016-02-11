bee35698a5fc9895df7c62e8dcba46167050f478
Michael Benaim <michael.benaim@external.grdf.fr>
2016-02-11 10:53:03 +0100

    Added Login simulation

--- src/PortalBundle/Controller/DefaultController.php
+++ src/PortalBundle/Controller/DefaultController.php
@@ -2,9 +2,13 @@
 
 namespace PortalBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultController extends FOSRestController
 {
@@ -21,6 +25,14 @@ class DefaultController extends FOSRestController
      */
     public function indexAction()
     {
-        return 'Bienvenue sur le Portail GDI';
+        $firewall = 'secured_area';
+
+        /** @var EntityManager $em */
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $user = $em->getRepository('UserBundle:User')->findAll();
+        $token = new UsernamePasswordToken($user[0], null, $firewall, array('ROLE_ADMIN'));
+        $this->get('security.token_storage')->setToken($token);
+        return $user[0];
     }
 }
