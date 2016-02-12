ccad38cf8d2139706f478c2f3ca991f99c618e7d
gueye <babacar.gueye@external.grdf.fr>
2016-02-12 16:11:09 +0100

    UserBundle UserType: enabling user during creation

--- src/UserBundle/Form/UserType.php
+++ src/UserBundle/Form/UserType.php
@@ -5,12 +5,10 @@ namespace UserBundle\Form;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use UserBundle\Entity\User;
-use UserBundle\Enum\RolesEnum;
 
 class UserType extends AbstractType
 {
