7697f535f95d2f76e08b012d18a95c565749381b
gueye <babacar.gueye@external.grdf.fr>
2016-02-12 09:55:13 +0100

    PortalBundle Agency, Role, Region Controller

---
+++ src/PortalBundle/Controller/RegionController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PortalBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use PortalBundle\Service\RegionService;
+use JMS\DiExtraBundle\Annotation as DI;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+/**
+ * Region controller
+ * @RouteResource("Region")
+ */
+class RegionController
+{
+    /**
+     * @var RegionService
+     * @DI\Inject("portal.service.region")
+     */
+    protected $regionService;
+    /**
+     * Provide Region's label.
+     * @Rest\Get("/regions")
+     * @Rest\View
+     *
+     * @ApiDoc(
+     *      section = "Region",
+     *      resource = true,
+     *      description = "permet de récuperer le label dune region"
+     * )
+     */
+    public function RegionLabelAction()
+    {
+        return $this->regionService->getRegionLabel();
+    }
+}
