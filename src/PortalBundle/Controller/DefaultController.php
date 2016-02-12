adcbdf3d3d6bf0df5a9c51802c99f29bfe9ddc7a
Michael Benaim <michael.benaim@external.grdf.fr>
2016-02-12 14:05:47 +0100

    Added security

--- src/PortalBundle/Controller/DefaultController.php
+++ src/PortalBundle/Controller/DefaultController.php
@@ -2,37 +2,87 @@
 
 namespace PortalBundle\Controller;
 
-use Doctrine\ORM\EntityManager;
 use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\HttpFoundation\Cookie;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use UserBundle\Service\UserService;
+use JMS\DiExtraBundle\Annotation as DI;
 
+/**
+ * Class DefaultController
+ * @package PortalBundle\Controller
+ */
 class DefaultController extends FOSRestController
 {
     /**
-     * Homepage
-     * @Rest\Get("/")
+     * @var UserService
+     * @DI\Inject("portal.service.user")
+     */
+    protected $userService;
+    /**
+     * @var TokenStorage
+     * @DI\Inject("security.token_storage")
+     */
+    protected $tokenStorage;
+
+    /**
+     * Fake Login
+     * @Rest\Get("/fake/login/{userId}")
      * @Rest\View
      *
      * @ApiDoc(
-     *      section = "Home",
+     *      section = "Login",
      *      resource = true,
-     *      description = "Homepage de portail"
+     *      description = "Fake login"
      * )
+     * @param $userId
+     * @return \UserBundle\Entity\User
      */
-    public function indexAction()
+    public function fakeLoginAction($userId)
     {
         $firewall = 'secured_area';
+        $user = $this->userService->get($userId);
+        $token = new UsernamePasswordToken($user, null, $firewall, $user->getRoles());
+        $this->tokenStorage->setToken($token);
+        return $this->tokenStorage->getToken()->getUser();
+    }
 
-        /** @var EntityManager $em */
-        $em = $this->get('doctrine.orm.entity_manager');
+    /**
+     * Fake Logout
+     * @Rest\Get("/fake/logout")
+     * @Rest\View
+     *
+     * @ApiDoc(
+     *      section = "Login",
+     *      resource = true,
+     *      description = "Fake logout"
+     * )
+     * @return \UserBundle\Entity\User
+     */
+    public function fakeLogoutAction()
+    {
+        $firewall = 'secured_area';
+        $this->tokenStorage->setToken(new AnonymousToken($firewall, 'anon.'));
+        return $this->tokenStorage->getToken()->getUser();
+    }
 
-        $user = $em->getRepository('UserBundle:User')->findAll();
-        $token = new UsernamePasswordToken($user[2], null, $firewall, array('ROLE_VISITEUR'));
-        $this->get('security.token_storage')->setToken($token);
-        return $user[2];
+    /**
+     * Who am I
+     * @Rest\Get("/fake/whoami")
+     * @Rest\View
+     *
+     * @ApiDoc(
+     *      section = "Login",
+     *      resource = true,
+     *      description = "Who am I"
+     * )
+     * @return \UserBundle\Entity\User
+     */
+    public function whoamiAction()
+    {
+        return $this->tokenStorage->getToken()->getUser();
     }
 }
