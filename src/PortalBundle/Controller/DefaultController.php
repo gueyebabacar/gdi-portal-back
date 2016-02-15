2b9466e191aa2073f10595e37caf007b9dba6b42
Michael Benaim <michael.benaim@external.grdf.fr>
2016-02-15 17:13:34 +0100

    Deleted login simulation

--- src/PortalBundle/Controller/DefaultController.php
+++ src/PortalBundle/Controller/DefaultController.php
@@ -18,60 +18,14 @@ use JMS\DiExtraBundle\Annotation as DI;
 class DefaultController extends FOSRestController
 {
     /**
-     * @var UserService
-     * @DI\Inject("portal.service.user")
-     */
-    protected $userService;
-    /**
      * @var TokenStorage
      * @DI\Inject("security.token_storage")
      */
     protected $tokenStorage;
 
     /**
-     * Fake Login
-     * @Rest\Get("/fake/login/{userId}")
-     * @Rest\View
-     *
-     * @ApiDoc(
-     *      section = "Login",
-     *      resource = true,
-     *      description = "Fake login"
-     * )
-     * @param $userId
-     * @return \UserBundle\Entity\User
-     */
-    public function fakeLoginAction($userId)
-    {
-        $firewall = 'secured_area';
-        $user = $this->userService->get($userId);
-        $token = new UsernamePasswordToken($user, null, $firewall, $user->getRoles());
-        $this->tokenStorage->setToken($token);
-        return $this->tokenStorage->getToken()->getUser();
-    }
-
-    /**
-     * Fake Logout
-     * @Rest\Get("/fake/logout")
-     * @Rest\View
-     *
-     * @ApiDoc(
-     *      section = "Login",
-     *      resource = true,
-     *      description = "Fake logout"
-     * )
-     * @return \UserBundle\Entity\User
-     */
-    public function fakeLogoutAction()
-    {
-        $firewall = 'secured_area';
-        $this->tokenStorage->setToken(new AnonymousToken($firewall, 'anon.'));
-        return $this->tokenStorage->getToken()->getUser();
-    }
-
-    /**
      * Who am I
-     * @Rest\Get("/fake/whoami")
+     * @Rest\Get("/user/whoami")
      * @Rest\View
      *
      * @ApiDoc(
