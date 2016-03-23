f1a1fcbb8a5db377d40e553b47d0695beb2af225
gueye <babacar.gueye@external.grdf.fr>
2016-03-23 10:58:29 +0100

    bin: reinit_recette.sh: add import user csv

--- bin/reinit_recette.sh
+++ bin/reinit_recette.sh
@@ -16,7 +16,7 @@ php app/console doctrine:schema:create -e recette
 php app/console cache:clear -e recette
 php app/console assets:install web --symlink -e recette
 
-php app/console csv:import:portal:
-php app/console csv:import:users:
+php app/console csv:import:portal -e recette
+php app/console csv:import:users -e recette
 
 echo "--- Fin reinit ---"
