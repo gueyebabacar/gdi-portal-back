8e221b348585fb037407fbc9faaadb04c108045d
gueye <babacar.gueye@external.grdf.fr>
2016-03-23 10:03:53 +0100

    PortalBundle: Tests: Service: ImportCsvServiceTest: skipping import AgenciesService test

---
+++ src/PortalBundle/Tests/Service/ImportCsvServiceTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace PortalBundle\Tests\Service;
+
+use Doctrine\ORM\EntityRepository;
+use Prophecy\Prophet;
+use Prophecy\Argument;
+use PortalBundle\Entity\Agency;
+use PortalBundle\Entity\Region;
+use Doctrine\ORM\EntityManager;
+use Prophecy\Prophecy\ObjectProphecy;
+use Symfony\Component\HttpKernel\Kernel;
+use PortalBundle\Service\ImportCsvService;
+
+class ImportCsvServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Prophet
+     */
+    private $prophet;
+
+    /**
+     * @var ObjectProphecy
+     */
+    private $emProphecy;
+
+    /**
+     * @var ObjectProphecy
+     */
+    private $kernelProphecy;
+
+    /**
+     * @var ImportCsvService
+     */
+    private $importCsvService;
+
+    /**
+     * setUp
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->prophet = new Prophet();
+
+        $this->emProphecy = $this->prophet->prophesize(EntityManager::class);
+        /** @var EntityManager $em */
+        $em = $this->emProphecy->reveal();
+
+        $this->kernelProphecy = $this->prophet->prophesize(Kernel::class);
+        /** @var Kernel $kernel */
+        $kernel = $this->kernelProphecy->reveal();
+
+        $this->importCsvService = new ImportCsvService($em, $kernel);
+    }
+
+    /**
+     * @test testGetPath
+     */
+    public function testGetPath()
+    {
+        $this->kernelProphecy
+            ->getRootDir()
+            ->willReturn(string::class)
+            ->shouldBeCalled();
+        $this->importCsvService->getPath();
+    }
+    /**
+     * @test testImportCsvAgences
+     */
+    public function testImportCsvAgences()
+    {
+        $this->markTestSkipped();
+        $agency = new Agency();
+//        $region = new Region();
+        $data = [
+            'code' => 'REG1',
+            'label' => 'Region 1',
+            'region' => 1,
+        ];
+
+        $repositoryProphecy = $this->prophet->prophesize(EntityRepository::class);
+
+        $agency->setCode($data['code']);
+        $agency->setLabel($data['label']);
+        $agency->setRegion($data['region']);
+
+        $this->assertEquals($agency->getCode(),$data['code']);
+        $this->assertEquals($agency->getLabel(),$data['label']);
+        $this->assertEquals($agency->getRegion(),$data['region']);
+
+        $this->emProphecy
+            ->getRepository('PortalBundle:Agency')
+            ->willReturn($repositoryProphecy)
+            ->shouldBeCalled();
+
+        $repositoryProphecy
+            ->findAll()
+            ->willReturn([$agency])
+            ->shouldBeCalled();
+
+        $this->emProphecy
+            ->persist($agency)
+            ->shouldBeCalled();
+
+        $this->emProphecy
+            ->flush()
+            ->shouldBeCalled();
+
+        $data = [
+            'path' => '/var/www/app/Resources/csv'
+        ];
+
+        $this->importCsvService->importCsvAgences($data['path']);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+}
